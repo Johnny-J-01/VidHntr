eb97f07aa8d4022003a3847c6abc92eb2e772b47
Ashmit Patil <ashtitan0001@gmail.com>
2026-09-03 16:23:30 +0530

    fix: connected FE req to the BE server

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import dotenv from "dotenv";
 
 import videosRouter from "./routes/videos.js";
+
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 8787;
